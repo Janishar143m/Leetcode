fff5dfa6c5448a2df1d3dce17244f6e0c522dd52
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-01-19 09:14:45 +0530

    Time: 0 ms (100.00%), Space: 37.5 MB (73.89%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,30 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int len=digits.length;
-        List<Integer> res=new ArrayList<>();
-        int i=digits.length-1;
-        digits[len-1]=digits[len-1]+1;
-       while(i>0)
-       {
-           if(digits[i]>9)
-           {
-               res.add(0,digits[i]-10);
-               digits[i]=digits[i]-10;
-               digits[i-1]=digits[i-1]+1;
-           }
-           else
-               res.add(0,digits[i]);
-           i--;
-       }
-        if(digits[0]>9)
-        {
-            res.add(0,digits[0]-10);
-            res.add(0,1);
+         int n = digits.length;
+    for(int i=n-1; i>=0; i--) {
+        if(digits[i] < 9) {
+            digits[i]++;
+            return digits;
         }
-        else
-            res.add(0,digits[0]);
         
-        return res.stream().mapToInt(k->k).toArray();
+        digits[i] = 0;
+    }
+    
+    int[] newNumber = new int [n+1];
+    newNumber[0] = 1;
+    
+    return newNumber;
      
         
     }
