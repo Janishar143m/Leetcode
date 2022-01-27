d904f06db6d8246f34e2f3426c56555fe50f211e
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-01-27 09:12:14 +0530

    Time: 1 ms (38.38%), Space: 41.7 MB (6.46%) - LeetHub

--- 794-valid-tic-tac-toe-state/794-valid-tic-tac-toe-state.java
+++ 794-valid-tic-tac-toe-state/794-valid-tic-tac-toe-state.java
@@ -1,85 +1,56 @@
 class Solution {
-    char[][] pos=new char[3][3];
-    
-    public boolean xStatus(int i,int j,char X)
-    {
-        int k=0,ct=0;
-        //xWin=true;
-        //Check for row
-        for(k=0;k<3;k++)
-            if(pos[i][k]==X)
-            {
-                ct++;
-            }
-        if(ct==3)
-            return true;
-        ct=0;
-        //Check for column
-        for(k=0;k<3;k++)
-            if(pos[k][j]==X)
-                ct++;
-        if(ct==3)
-            return true;
-        ct=0;
-        //Check for diagonals
-        for(k=0;k<3;k++)
-        {
-            if(pos[k][k]==X)
-                ct++;
-        }
-         if(ct==3)
-            return true;
-        ct=0;
-        //Check for backward diagonals
-        for(k=0;k<=2;k++)
-        {
-            if(pos[k][2-k]==X)
-                ct++;
-        }
-        
-        if(ct==3)
-         return true;
-        else
-            return false;
-    }
     public boolean validTicTacToe(String[] board) {
-        int countX=0,countO=0;
+        int rows[]=new int[3];
+        int cols[]=new int[3];
+        int diag=0,antidiag=0,turn=0;
         boolean xWin=false,yWin=false;
         for(int i=0;i<3;i++)
         {
-            String s=board[i];
-
-                for (int j = 0; j < 3; j++) {
-                    pos[i][j] = s.charAt(j);
-                    if (pos[i][j] == 'X') {
-                        countX++;
-                        if(!xWin)
-                        {
-                            xWin = xStatus(i, j,'X');
-                        }
-                    } else if (pos[i][j] == 'O')
-                    {
-                        countO++;
-                        if(!yWin)
-                        {
-                            yWin = xStatus(i, j,'O');
-                        }
-                  }
+            char[] c=board[i].toCharArray();
+            for(int j=0;j<3;j++)
+            {
+                if(c[j]=='X')
+                {
+                    rows[i]++;
+                    cols[j]++;
+                    turn++;
+                }
+                if(c[j]=='O')
+                {
+                    rows[i]--;
+                    cols[j]--;
+                    turn--;
+                }
+                if(i==j)
+                {
+                     if(c[j]=='X')
+                        diag++;
+                    else if(c[j]=='O')
+                        diag--;   
+                }  
+                if(i+j==2)
+                {
+                    if(c[j]=='X')
+                        antidiag++;
+                    else if(c[j]=='O')
+                        antidiag--;
+                }
+                    
             }
-        }     
-
-       
-
-        if(countX==0 && countO==0)
-            return true;
-        else if(countX==countO && xWin==false && yWin==false)
-            return true;
-        else if(countX==countO+1 && yWin==false)
-            return true;
-          else if(countX==countO && xWin==false && yWin==true)
-            return true;    
-        else
-            return false;
-   
+        }
+        
+        if(rows[0]==3||rows[1]==3||rows[2]==3||cols[0]==3||cols[1]==3||cols[2]==3||diag==3||antidiag==3)
+            xWin=true;
+  if(rows[0]==-3||rows[1]==-3||rows[2]==-3||cols[0]==-3||cols[1]==-3||cols[2]==-3||diag==-3||antidiag==-3)
+            yWin=true;
+ 
+if((xWin==true && yWin==false && turn==1)||(yWin==true && turn==0 && xWin==false))
+    return true;
+else if(xWin==false && yWin==false && (turn==0||turn==1))
+    return true;
+ else
+     return false;
+        
+        
+    }
 }
-}    
