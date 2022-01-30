6531a8bca1f6b82c015c61f23aadf7c0f9687a7e
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-01-30 16:50:42 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (8.28%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,72 +10,21 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null)
+            return list2;
+        if(list2==null)
+            return list1;
+        if(list1.val<list2.val)
+        {
+            list1.next=mergeTwoLists(list1.next,list2);
+            return list1;
+        }
+        else
+        {
+            list2.next=mergeTwoLists(list1,list2.next);
+            return list2;
+        }
         
-        ListNode node1=list1;
-        ListNode node2=list2;
-        ListNode result=null;
-        ListNode resultStart=result;
-        while(node1!=null && node2!=null)
-        {
-            if(node1.val<node2.val)
-            {
-             if(result==null)
-             {
-                 result=node1;
-                 resultStart=result;
-             }   
-                else
-                {
-                    result.next=node1;
-                    result=result.next;
-                }   
-              node1=node1.next;                              
-            }
-            else
-            {
-                if(result==null)
-                {
-                  result=node2;
-                  resultStart=result;
-                } 
-                else
-                {
-                    result.next=node2;
-                    result=result.next;
-                }   
-              node2=node2.next;                              
-            }
-            
-        }
-        while(node1!=null)
-        {
-            if(result==null)
-             {
-                 result=node1;
-                 resultStart=result;
-             }   
-                else
-                {
-                    result.next=node1;
-                    result=result.next;
-                }   
-              node1=node1.next;   
-        }
-        while(node2!=null)
-        {
-            if(result==null)
-             {
-                 result=node2;
-                 resultStart=result;
-             }   
-                else
-                {
-                    result.next=node2;
-                    result=result.next;
-                }   
-              node2=node2.next;   
-        }
-        return resultStart;
         
     }
 }
