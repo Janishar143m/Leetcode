d5bd416e9ae7383ac79b0b8d5cb00473db07ec7b
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-02-05 09:12:32 +0530

    Time: 46 ms (31.81%), Space: 68.8 MB (54.21%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -9,17 +9,18 @@
  * }
  */
 class Solution {
-   public int[] nextLargerNodes(ListNode head) {
-        ArrayList<Integer> A = new ArrayList<>();
-        for (ListNode node = head; node != null; node = node.next)
-            A.add(node.val);
-        int[] res = new int[A.size()];
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < A.size(); ++i) {
-            while (!stack.isEmpty() && A.get(stack.peek()) < A.get(i))
-                res[stack.pop()] = A.get(i);
-            stack.push(i);
-        }
-        return res;
+    public int[] nextLargerNodes(ListNode head) {
+        Stack<Integer>stack=new Stack<Integer>();
+        ArrayList<Integer>list=new ArrayList<>();
+        for (ListNode node=head;node!=null;node=node.next)
+            list.add(node.val);
+        int result[]=new int[list.size()];
+       for (int i=0 ;i<list.size();i++)
+       {
+           while(!stack.isEmpty() && list.get(stack.peek())<list.get(i))
+               result[stack.pop()]=list.get(i);
+           stack.push(i);
+       }
+        return result;  
     }
 }
