2891b0070653364f3e4701d9f20b0a778db1e703
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-03-03 10:19:54 +0530

    Time: 210 ms (12.06%), Space: 99.3 MB (46.10%) - LeetHub

--- 911-online-election/911-online-election.java
+++ 911-online-election/911-online-election.java
@@ -1,57 +1,37 @@
 class TopVotedCandidate {
     int[]persons;
     int[]times;
-
+   TreeMap<Integer,Integer>treeMap;
    public TopVotedCandidate(int[] persons, int[] times) {
         this.times=times;
         int[] maxVotes=new int[persons.length];
         this.persons=new int[persons.length];
         int max=0,maxp=-1;
-        //Map<Integer,Integer>map=new HashMap<>();
+        treeMap=new TreeMap<Integer,Integer>();
         for(int i=0;i<times.length;i++)
         {
-           // max=-1;
-           // maxp=-1;
-
-            maxVotes[persons[i]]++;
-            //map.put(persons[i],i);
+             maxVotes[persons[i]]++;
            if(maxVotes[persons[i]]>=max) 
            {
                max=maxVotes[persons[i]];
                maxp=persons[i];
            } 
-
-            /*for(int j=0;j<maxVotes.length;j++)
-            {
-                if(maxVotes[j]>max)
-                {
-                    max=maxVotes[j];
-                    maxp=j;
-                }
-                    if(maxVotes[j]==max)
-                    {
-                        if(map.getOrDefault(j,-1)>map.getOrDefault(maxp,-1))
-                        {
-                            max=maxVotes[j];
-                            maxp=j;
-                        }
-                    }
-            }*/
             this.persons[i]=maxp;
+            treeMap.put(times[i],maxp);
         }
     }
 
     public int q(int t) {
-        int i;
+        /*int i;
         for(i=1;i<persons.length;i++)
         {
             if(times[i-1]<=t && times[i]>t)
                 return persons[i-1];
             else if(times[i]==t)
                 return persons[i];
-        }
+        }*/
 
-        return this.persons[persons.length-1];
+        return this.treeMap.floorEntry(t).getValue();
 
 
     }
