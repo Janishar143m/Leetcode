cb786acf10a38c891489fa19b479325367299e4a
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-03-20 07:17:25 +0530

    Time: 62 ms (5.06%), Space: 62.9 MB (5.03%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result;
+    public int pathSum(TreeNode root, int targetSum) {
+        //List<TreeNode> pathList=new ArrayList<>();
+       // int result;
+        if(root==null)
+            return 0;
+        Queue<TreeNode> queue=new ArrayDeque<>();
+        queue.add(root);
+        while(queue.size()>0)
+        {
+            TreeNode node=queue.poll();
+             pathSumUtil(node,targetSum,new ArrayList<>());
+            if(node.left!=null)
+                queue.offer(node.left);
+            if(node.right!=null)
+                queue.offer(node.right);
+        }
+        return result;
+        
+    }
+    private void pathSumUtil(TreeNode root,int targetSum,List<TreeNode> list)
+    {
+        if(root==null)
+            return;
+        list.add(root);
+        int newSum=targetSum-root.val;
+        if(newSum==0)
+            result++;
+
+       pathSumUtil(root.left,newSum,list);
+       pathSumUtil(root.right,newSum,list);
+       
+        list.remove(list.size()-1);
+    }
+}
