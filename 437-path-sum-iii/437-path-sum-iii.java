5d42a2ac7a4c5c94f35857cdccb60a9dfea33774
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-03-21 09:39:49 +0530

    Time: 22 ms (28.84%), Space: 41.6 MB (84.94%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -1,36 +1,23 @@
 class Solution {
-    int result;
-    public int pathSum(TreeNode root, int targetSum) {
-        //List<TreeNode> pathList=new ArrayList<>();
-       // int result;
-        if(root==null)
-            return 0;
-        Queue<TreeNode> queue=new ArrayDeque<>();
-        queue.add(root);
-        while(queue.size()>0)
-        {
-            TreeNode node=queue.poll();
-             pathSumUtil(node,targetSum,new ArrayList<>());
-            if(node.left!=null)
-                queue.offer(node.left);
-            if(node.right!=null)
-                queue.offer(node.right);
-        }
-        return result;
-        
+    private int cnt;
+    
+    public int pathSum(TreeNode root, int sum) {
+        cnt = 0;
+        find(root, sum);
+        return cnt;
     }
-    private void pathSumUtil(TreeNode root,int targetSum,List<TreeNode> list)
-    {
-        if(root==null)
-            return;
-        list.add(root);
-        int newSum=targetSum-root.val;
-        if(newSum==0)
-            result++;
-
-       pathSumUtil(root.left,newSum,list);
-       pathSumUtil(root.right,newSum,list);
-       
-        list.remove(list.size()-1);
+    
+    private void find(TreeNode root, int sum) {
+        if (root == null) return;
+        path(root, sum);
+        find(root.left, sum);
+        find(root.right, sum);
+    }
+    
+    private void path(TreeNode root, int sum) {
+        if (root == null) return;
+        if (sum - root.val == 0) cnt++;
+        path(root.left, sum - root.val);
+        path(root.right, sum - root.val);
     }
 }
