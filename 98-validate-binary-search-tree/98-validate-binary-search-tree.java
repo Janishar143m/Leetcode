47979afeaab6b92ad87608867fee80d3084ecdbd
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-03-28 13:33:33 +0530

    Time: 1 ms (57.19%), Space: 43.8 MB (55.52%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -1,28 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-     TreeNode prev=null;
+    TreeNode prev=null;
     public boolean isValidBST(TreeNode root) {
-       
-        if(root==null)
-            return true;
-        if(root.left==null && root.right==null)
-            return true;
-        return isValidBSTUtil(root);
-        
+        boolean result;
+        result=isValidBSTUtil(root);
+        return result;
     }
     public boolean isValidBSTUtil(TreeNode root)
     {
         if(root==null)
             return true;
-        boolean ls=true,rs=true;
-          ls=isValidBSTUtil(root.left);
-        if(ls==false)
-            return false;
+        boolean leftBST=isValidBSTUtil(root.left);
         if(prev!=null && prev.val>=root.val)
             return false;
         prev=root;
-          rs=isValidBSTUtil(root.right);
-        if(rs==false)
-            return false;
-        return true;
+        boolean rightBST=isValidBSTUtil(root.right);
+        return leftBST&&rightBST;
     }
 }
