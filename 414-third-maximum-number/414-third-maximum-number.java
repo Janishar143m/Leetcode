07ec4518df1d5492b5ec0b7ab0ff139d92eb940c
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-04-02 08:39:19 +0530

    Time: 322 ms (5.01%), Space: 45.3 MB (10.98%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.java
+++ 414-third-maximum-number/414-third-maximum-number.java
@@ -1,33 +1,18 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Integer a=null;
-        Integer b=null;
-        Integer c=null;
-        for(int i=0;i<nums.length;i++)
+        int max=Integer.MIN_VALUE;
+        Queue<Integer> queue=new PriorityQueue<>(Collections.reverseOrder());
+        Arrays.stream(nums).filter(i->!queue.contains(i)).forEach(i->queue.offer(i));
+        int length=queue.size();
+        if(length>2)
         {
-            if((a!=null && nums[i]==a)||(b!=null && nums[i]==b)||(c!=null && nums[i]==c))
-                continue;
-            
-            if(a==null||nums[i]>a)
-            {
-                c=b;
-                b=a;
-                a=nums[i];
-            }
-            else if(b==null||nums[i]>b)
-            {
-                c=b;
-                b=nums[i];
-            }
-            else if(c==null||nums[i]>c)
-                c=nums[i];
-           
-                
+           queue.poll();
+           queue.poll();
+           max=queue.poll(); 
         }
-        if(c==null)
-            return a;
-          else  
-        return c;
+        else
+            max=queue.poll();
+        return max;
         
     }
 }
