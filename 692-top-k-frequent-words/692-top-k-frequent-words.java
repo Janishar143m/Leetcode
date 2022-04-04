8992205810af8f003e67f35a91317f93cd488e43
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-04-04 08:12:23 +0530

    Time: 8 ms (74.87%), Space: 45.6 MB (22.09%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -1,16 +1,30 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-         Map<String,Integer> map=new HashMap<>();
-        Queue<String> queue=new PriorityQueue<>((a,b)->map.get(b)-map.get(a)==0?a.compareTo(b):map.get(b)-map.get(a));
-       
-        for(String s:words)
-            map.put(s,map.getOrDefault(s,0)+1);
-        for(Map.Entry entry:map.entrySet())
-            queue.offer((String)entry.getKey());
-        List<String> result=new ArrayList<>();
-        for(int i=0;i<k;i++)
-            result.add(queue.poll());
-        return result;
         
+        List<String> result = new LinkedList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<words.length; i++)
+        {
+            if(map.containsKey(words[i]))
+                map.put(words[i], map.get(words[i])+1);
+            else
+                map.put(words[i], 1);
+        }
+        
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+        );
+        
+        for(Map.Entry<String, Integer> entry: map.entrySet())
+        {
+            pq.offer(entry);
+            if(pq.size()>k)
+                pq.poll();
+        }
+
+        while(!pq.isEmpty())
+            result.add(0, pq.poll().getKey());
+        
+        return result;
     }
 }
