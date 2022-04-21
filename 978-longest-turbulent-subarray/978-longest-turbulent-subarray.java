4a237392ff4b298d195a45b7c7238cdaef92649c
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-04-21 09:46:55 +0530

    Time: 4 ms (97.77%), Space: 46.4 MB (96.44%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.java
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.java
@@ -2,52 +2,41 @@ class Solution {
     public int maxTurbulenceSize(int[] arr) {
         boolean smaller=false,greater=false;
         int maxLength=0;
-        int[] dp=new int[arr.length];
+        int ct=0;
         if(arr.length==1)
             return 1;
-        if(arr[0]<arr[1])
+        for(int i=1;i<arr.length;i++)
         {
-            dp[0]=1;
-            smaller=true;
-        }
-        else if(arr[0]>arr[1])
-        {
-            dp[0]=1;
-            greater=true;
-        }
-        maxLength=Math.max(maxLength,dp[0]);
-        for(int i=1;i<arr.length-1;i++)
-        {
-            if(arr[i]>arr[i+1])
+            if(arr[i]>arr[i-1])
             {
                if(smaller) 
                {
-                   dp[i]=dp[i-1]+1;
+                   ct++;
                    smaller=false;
                }   
                 else
-                    dp[i]=1;
+                    ct=1;
                 greater=true;
                 
             }    
-            else if(arr[i]<arr[i+1])
+            else if(arr[i]<arr[i-1])
             {
                if(greater) 
                {
-                   dp[i]=dp[i-1]+1;
+                   ct++;
                    greater=false;
                }
                else
-                   dp[i]=1;
+                   ct=1;
                smaller=true; 
             }   
             else
             {
-                dp[i]=0;
+                ct=0;
                 smaller=false;
                 greater=false;
             }
-            maxLength=Math.max(maxLength,dp[i]);
+            maxLength=Math.max(maxLength,ct);
             
         }
         return maxLength+1;
