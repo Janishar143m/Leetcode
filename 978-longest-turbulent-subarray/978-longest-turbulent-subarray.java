674330faee48091f4f26903f7e4f05c9a0753b35
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-04-21 09:29:23 +0530

    Time: 7 ms (43.43%), Space: 69.3 MB (9.35%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.java
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.java
@@ -1,54 +1,56 @@
 class Solution {
     public int maxTurbulenceSize(int[] arr) {
-        int ct=0,maxValue=0;
-        boolean greater=false,smaller=false;
-        for(int i=0;i<arr.length-1;i++)
+        boolean smaller=false,greater=false;
+        int maxLength=0;
+        int[] dp=new int[arr.length];
+        if(arr.length==1)
+            return 1;
+        if(arr[0]<arr[1])
         {
-            if(arr[i]==arr[i+1])
+            dp[0]=1;
+            smaller=true;
+        }
+        else if(arr[0]>arr[1])
+        {
+            dp[0]=1;
+            greater=true;
+        }
+        maxLength=Math.max(maxLength,dp[0]);
+        for(int i=1;i<arr.length-1;i++)
+        {
+            if(arr[i]>arr[i+1])
             {
+               if(smaller) 
+               {
+                   dp[i]=dp[i-1]+1;
+                   smaller=false;
+               }   
+                else
+                    dp[i]=1;
+                greater=true;
+                
+            }    
+            else if(arr[i]<arr[i+1])
+            {
+               if(greater) 
+               {
+                   dp[i]=dp[i-1]+1;
+                   greater=false;
+               }
+               else
+                   dp[i]=1;
+               smaller=true; 
+            }   
+            else
+            {
+                dp[i]=0;
                 smaller=false;
                 greater=false;
-                ct=0;
             }
-            else if(!greater && !smaller)
-            {
-               if(arr[i]<arr[i+1])
-                {
-                    ct++;
-                    smaller=true;
-                }
-                else if(arr[i]>arr[i+1])
-                {
-                    ct++;
-                    greater=true;
-                }
-            }
-            else if(greater)
-            {
-                if(arr[i]<arr[i+1])
-                {
-                    ct++;
-                    smaller=true;
-                    greater=false;
-                }
-                else
-                    ct=1;
-                    
-            }
-            else if(smaller)
-            {
-                 if(arr[i]>arr[i+1])
-                {
-                    ct++;
-                    greater=true;
-                     smaller=false;
-                }
-                else
-                    ct=1;
-            }
-            maxValue=Math.max(maxValue,ct);
+            maxLength=Math.max(maxLength,dp[i]);
             
         }
-        return maxValue+1;
+        return maxLength+1;
+        
     }
 }
