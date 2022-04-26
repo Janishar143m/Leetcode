9796393301e28b71ce68547186fef618b0a5f6da
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-04-26 07:45:51 +0530

    Time: 318 ms (7.90%), Space: 49.3 MB (57.50%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,10 +2,11 @@ class Solution {
     public int jump(int[] nums) {
         
         int[] minSteps=new int[nums.length];
-        Arrays.fill(minSteps,100000000);
+        //Arrays.fill(minSteps,100000000);
         minSteps[0]=0;
         for(int i=1;i<nums.length;i++)
         {
+            minSteps[i]=100000000;
             for(int j=0;j<i;j++)
                 if(nums[j]>=i-j)
                     minSteps[i]=Math.min(minSteps[i],1+minSteps[j]);
