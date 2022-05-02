b055fa585749c44a1f689c2bb3b1c0ec37863118
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-05-02 23:54:36 +0530

    Time: 13 ms (19.89%), Space: 58.5 MB (41.58%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,36 +1,18 @@
 class Solution {
-    public int maximalSquare(char[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int max=-1;
-        int sum[][]=new int[m+1][n+1];
-        for(int i=0;i<n;i++)
-        {
-            sum[0][i]=Character.getNumericValue(matrix[0][i]);
-            max=Math.max(max,sum[0][i]);
-        }   
-        for(int i=0;i<m;i++)
-        {
-            sum[i][0]=Character.getNumericValue(matrix[i][0]);
-            max=Math.max(max,sum[i][0]);
-        }   
-        for(int i=1;i<m;i++)
-            for(int j=1;j<n;j++)
+    public int maximalSquare(char[][] matrix) 
+    {
+        int max=0;
+        int[][] dp=new int[matrix.length][matrix[0].length];
+        for(int i=0;i<dp.length;i++)
+            for(int j=0;j<dp[0].length;j++)
             {
-                //System.out.println(max);
-                if(matrix[i][j]=='1')
-                {
-                    sum[i][j]=Math.min(sum[i-1][j-1],Math.min(sum[i][j-1],sum[i-1][j]))+1;
-                    //System.out.println("i "+i+"j "+j+":sum[i][j]"+sum[i][j]);
-                    max=Math.max(max,sum[i][j]);
-                    //System.out.println("i "+i+"j "+j+":max"+max);
-                }  
-                else
-                    sum[i][j]=0;
-            }
-        //System.out.println(Arrays.toString(sum));
+                if(i==0||j==0||Character.getNumericValue(matrix[i][j])==0)
+                    dp[i][j]=Character.getNumericValue(matrix[i][j]);
+                else 
+                    dp[i][j]=(Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j])))+1;
+                max=Math.max(max,dp[i][j]);
+    
+             }
         return max*max;
     }
-}
-
-
+}    
