6cd2ad7173abcb74e144d08933ca1a397f36c009
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-05-12 10:18:33 +0530

    Time: 61 ms (51.62%), Space: 68.6 MB (20.03%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,46 +1,17 @@
 class Solution {
-    public boolean compare(String first,String second)
-    {
-        if(first.length()!=second.length()+1)
-            return false;
-        int i=0;
-        int j=0;
-        while(i<first.length())
-        {
-            if(j<second.length() && first.charAt(i)==second.charAt(j))
-            {
-                i++;
-                j++;
+    public int longestStrChain(String[] words) {
+        Map<String, Integer> dp = new HashMap<>();
+        Arrays.sort(words, (a, b)->a.length() - b.length());
+        int res = 0;
+        for (String word : words) {
+            int best = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
             }
-            else
-                i++;
+            dp.put(word, best);
+            res = Math.max(res, best);
         }
-        if(i==first.length() && j==second.length())
-            return true;
-        else
-            return false;
-    }
-    public int longestStrChain(String[] words) 
-    {
-        int max=0;
-        Arrays.sort(words,(s1,s2)->s1.length()-s2.length());
-        //Arrays.stream(words).forEach(System.out::println);
-        int[] dp=new int[words.length];
-        for(int i=0;i<dp.length;i++)
-            dp[i]=1;
-        //Arrays.fill(dp,1);
-       // max=dp[0];
-        for(int i=0;i<words.length;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                if(compare(words[i],words[j]) && dp[j]+1>dp[i])
-                    dp[i]=dp[j]+1;
-                
-            }
-            max=Math.max(max,dp[i]);
-        }    
-        return max;
-        
+        return res;
     }
 }
