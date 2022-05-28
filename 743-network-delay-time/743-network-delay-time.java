7a940ddfa53d88ab642a536ff680c76b6a16ff38
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-05-28 11:23:51 +0530

    Time: 33 ms (41.69%), Space: 64.4 MB (50.82%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -30,26 +30,22 @@ class Solution {
         queue.add(new HeapEntry(0,k));
         while(!queue.isEmpty())
         {
-            //System.out.println(queue.toString());
-           // System.out.println(Arrays.toString(weight));
+           
               entry=queue.poll();
-           // System.out.println("Popped:"+i);
             if(visited.contains(entry.getNode()))
                 continue;
            visited.add(entry.getNode());
            result=Math.max(result,entry.getWeight());
             ct++;    
-             //int minVal=0;
             for(int j=0;j<n;j++)
             {
                
                 if(adj[entry.getNode()][j]!=-1)
                 {
-                    //System.out.println(adj[i][j]);
                      int newWeight=entry.getWeight()+adj[entry.getNode()][j];
                     queue.offer(new HeapEntry(newWeight,j));
                   
-                    //System.out.println(weight[j]);
+                   
                 }
                
                
@@ -57,7 +53,6 @@ class Solution {
             
                 
         }
-     // System.out.println(ct);
         if(ct==n)
             return result;
         else
@@ -71,8 +66,6 @@ class Solution {
             Arrays.fill(adj[i],-1);
         int weight[]=new int[n];
         Set<Integer> visited=new HashSet<>();
-       // boolean visited[]=new boolean[n];
-        //Arrays.fill(visited,false);
         int result=0;
         Arrays.fill(weight,Integer.MAX_VALUE);
         weight[k-1]=0;
@@ -80,8 +73,6 @@ class Solution {
        {
            adj[times[i][0]-1][times[i][1]-1]=times[i][2];
        }
-      // for(int i=0;i<n;i++) 
-        //System.out.println(Arrays.toString(adj[i]));
         result=bfs(adj,weight,visited,n,k-1);
         return result;
         
