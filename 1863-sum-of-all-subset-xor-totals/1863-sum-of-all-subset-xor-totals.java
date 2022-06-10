cb0afe1da95b7a49a3d58829bf57f72142a5d4a0
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-06-10 09:30:14 +0530

    Time: 63 ms (5.26%), Space: 62.6 MB (5.13%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,14 +1,41 @@
 class Solution {
-    public int subsetSum(int i,int sum,int nums[])
+    public int subsetSum(int nums[])
     {
-        if(i==nums.length)
-            return sum;
-        else
-            return subsetSum(i+1,sum,nums)+subsetSum(i+1,sum^nums[i],nums);
+        List<List<Integer>> result=new ArrayList<>();
+        result.add(new ArrayList<>());
+        int sum=0;
+        //result.add(new ArrayList<>());
+       for(int i=0;i<nums.length;i++)
+       {
+           List<List<Integer>> current=new ArrayList<>();
+           for(List<Integer> list:result)
+               current.add(new ArrayList<>(list));
+          // System.out.println("Current list size:"+current.size());
+           for(List<Integer> list:current)
+           {
+               //System.out.println("List is:"+list.toString());
+               list.add(nums[i]);
+                //System.out.println("Result list before adding to result:"+result.toString());
+                result.add(new ArrayList<>(list)); 
+               //System.out.println("Listafter number addition is:"+result.toString());
+               //result.add(new ArrayList<>(list));
+           }
+               
+            
+           //System.out.println("result list after adding is:"+result.toString());
+              
+           
+       }
+        for(List<Integer>list:result)
+        {
+            //System.out.println(list.toString());
+            sum+=list.stream().reduce(0,(a,b)->a^b);
+        }   
+        return sum;
     }
     public int subsetXORSum(int[] nums) {
         int sum=0;
-        sum=subsetSum(0,0,nums);
+        sum=subsetSum(nums);
         return sum;
         
     }
