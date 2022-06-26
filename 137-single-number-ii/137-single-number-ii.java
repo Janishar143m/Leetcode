522263b76c1c44882eba17a978fc848df311da13
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-06-26 08:51:11 +0530

    Time: 1 ms (93.47%), Space: 43.7 MB (73.09%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int ans=0,sum=0;
-        for(int i=0;i<32;i++)
-        {
-            sum=0;
-            for(int j=0;j<nums.length;j++)
-            {
-                if((nums[j]>>i & 1)==1)
-                {
-                    sum++;
-                    sum%=3;
-                }
-            }
-            if(sum!=0)
-                ans|=sum<<i;
-        }
-        return ans;
+   public int singleNumber(int[] nums) {
+  int ones = 0, twos = 0, threes = 0;
         
+  for (int i = 0; i < nums.length; i++) {
+    // twos holds the num that appears twice
+    twos |= ones & nums[i];
+    
+    // ones holds the num that appears once
+    ones ^= nums[i];
+ 
+    // threes holds the num that appears three times
+    threes = ones & twos;
+            
+    // if num[i] appears three times
+    // doing this will clear ones and twos
+    ones &= ~threes;
+    twos &= ~threes;
+  }
         
-    }
+  return ones;
+}
 }
