58dd15e798b24d23eedca87f092944524d0a90bd
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-07-11 16:58:20 +0530

    Time: 37 ms (48.58%), Space: 58.7 MB (64.14%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,35 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
         Arrays.sort(nums);
         List<List<Integer>> result=new ArrayList<>();
         for(int i=0;i<nums.length-2;i++)
         {
-            if(i==0||(i>0 && nums[i]!=nums[i-1]))
+            if(i==0||i>0 && nums[i]!=nums[i-1])
             {
                 int lower=i+1;
-                int higher=nums.length-1;
-                int sum=0-nums[i];
-                while(lower<higher)
+                int upper=nums.length-1;
+                int sum=-nums[i];
+                while(lower<upper)
                 {
-                    if(nums[lower]+nums[higher]==sum)
+                    if(nums[lower]+nums[upper]==sum)
                     {
-                        result.add(Arrays.asList(nums[i],nums[lower],nums[higher]));
-                    while(lower<higher && nums[lower]==nums[lower+1])
-                         lower++;
-                     while(lower<higher && nums[higher]==nums[higher-1])
-                         higher--;
-                    lower++;
-                    higher--;
-                    }
-                    else if(nums[lower]+nums[higher]>sum)
-                        higher--;
-                    else
+                        result.add(Arrays.asList(nums[i],nums[lower],nums[upper]));
+                        while( lower<upper && nums[lower]==nums[lower+1])
+                            lower++;
+                        while(lower<upper && nums[upper]==nums[upper-1])
+                            upper--;
                         lower++;
-                    }   
+                        upper--;
+                    }  
+                    else if(nums[lower]+nums[upper]<sum)
+                        lower++;
+                    else
+                        upper--;
                 }
-            }
-        
+            } 
+                
+        }
         return result;
         
     }
