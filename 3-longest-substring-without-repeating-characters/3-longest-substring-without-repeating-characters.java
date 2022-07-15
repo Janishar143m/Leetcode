4fe037658def14fba172753c95455546c0e65157
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-07-15 09:31:02 +0530

    Time: 6 ms (85.40%), Space: 42.1 MB (96.66%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,32 +1,20 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0)
-            return 0;
-        int n=s.length();
-        int startWindow=0,endWindow=0;
         Map<Character,Integer> map=new HashMap<>();
-        //map.put(s.charAt(0),startWindow);
-       int maxLength=1;
-        for(endWindow=0;endWindow<n;endWindow++)
+        int maxLength=0;
+        int startWindow=0;
+        int n=s.length();
+        for(int endWindow=0;endWindow<n;endWindow++)
         {
-            if(!map.containsKey(s.charAt(endWindow)))
+            if(map.containsKey(s.charAt(endWindow)) && map.get(s.charAt(endWindow))+1>startWindow)
             {
-                map.put(s.charAt(endWindow),endWindow);
-                
-            }   
-            else
-            {
-                if(map.get(s.charAt(endWindow))>=startWindow)
-                     startWindow=map.get(s.charAt(endWindow))+1;
-                map.put(s.charAt(endWindow),endWindow);
-                //startWindow=map.get();    
+                startWindow=map.get(s.charAt(endWindow))+1;
             }
+            map.put(s.charAt(endWindow),endWindow);
             maxLength=Math.max(maxLength,endWindow-startWindow+1);
             
         }
-       // maxLength=Math.max(maxLength,endWindow-startWindow+1);
         return maxLength;
-        
         
     }
 }
