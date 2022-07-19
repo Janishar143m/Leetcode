fd007dd22e7609cc399364d8494c5f4f07cd02ec
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-07-19 09:18:42 +0530

    Time: 7 ms (79.67%), Space: 43.9 MB (31.70%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,65 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1 == null || s2 == null) {
-            throw new IllegalArgumentException("Input string is null");
-        }
-
-        int l1 = s1.length();
-        int l2 = s2.length();
-        if (l1 == 0) {
-            return true;
-        }
-        if (l2 < l1) {
+        
+        if(s1.length()>s2.length())
             return false;
+        int n1=s1.length();
+        int n2=s2.length();
+        int[] charFreq1=new int[26];
+        int[] charFreq2=new int[26];
+        for(int i=0;i<n1;i++)
+        {
+            charFreq1[s1.charAt(i)-'a']++;
+            charFreq2[s2.charAt(i)-'a']++;
+            
         }
-
-        int[] countMap = new int[26];
-
-        for (int i = 0; i < l1; i++) {
-            // Adding Characters of S1 in the window
-            countMap[s1.charAt(i) - 'a']++;
-            // Removing Characters of S2 in the window
-            countMap[s2.charAt(i) - 'a']--;
-        }
-
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-            // Counting the characters which have count as zero.
-            // Either these characters are not present in the window or appear same number
-            // of times in the window.
-            if (countMap[i] == 0) {
-                count++;
-            }
-        }
-        // If count is 26, all S1 characters appear same number of times in S2.
-        if (count == 26) {
+        if(Arrays.equals(charFreq1,charFreq2))
             return true;
+        for(int i=n1;i<n2;i++)
+        {
+            charFreq2[s2.charAt(i)-'a']++;
+            charFreq2[s2.charAt(i-n1)-'a']--;
+            if(Arrays.equals(charFreq1,charFreq2))
+             return true;
+
         }
-
-        for (int i = l1; i < l2; i++) {
-            // Adding new character in the window.
-            int r = s2.charAt(i) - 'a';
-            countMap[r]--;
-            if (countMap[r] == 0) {
-                count++;
-            } else if (countMap[r] == -1) {
-                count--;
-            }
-
-            // Removing old character from the window.
-            int l = s2.charAt(i - l1) - 'a';
-            countMap[l]++;
-            if (countMap[l] == 0) {
-                count++;
-            } else if (countMap[l] == 1) {
-                count--;
-            }
-
-            if (count == 26) {
-                return true;
-            }
-        }
-
         return false;
+        
     }
 }
