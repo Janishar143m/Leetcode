04b63255190d7818cf30df400416553ed1760bb0
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-08-19 13:38:02 +0530

    Time: 1 ms (76.46%), Space: 42.6 MB (49.12%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,64 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        int low=0,high=nums.length-1,resultPos=-1;
-        int index=searchPartitionIndex(nums);
-       //System.out.println(index);
-        if(index>0)
-        {
-           if(target>=nums[index] && target<nums[0])
-        {
-            
-            resultPos=searchPartition(nums,target,index,high);
-        }   
-        else
-        {
-            
-             resultPos=searchPartition(nums,target,low,index-1);
-        }  
-        }
-        else
-            resultPos=searchPartition(nums,target,low,high);
-         
-           
-            
-        return resultPos;
-        
-    }
-    
-    private int searchPartitionIndex(int[]nums)
-    {
-        int result=0;
+        int leftLow=0;
+        int endHigh=nums.length-1;
+        int midVal=0;
+        int low=0;
+        int high=0;
         for(int i=0;i<nums.length-1;i++)
-              if(nums[i]>nums[i+1])
-              {
-                   //System.out.println("Condition true");
-                  result=i+1; 
-              }
-                 
-        //System.out.println(i);
-        return result;
-          
-        
-            
-    }
-    private int searchPartition(int nums[],int target,int low,int high)
-    {
-        int mid=0,index=-1;
-        if(low<=high)
+            if(nums[i]>nums[i+1])
+                midVal=i;
+        if(target>=nums[leftLow] && target<=nums[midVal])
         {
-             mid=low+(high-low)/2;
-            if(nums[mid]==target)
-                index=mid;
-             else if(nums[mid]<target)
-                 index=searchPartition(nums,target,mid+1,high);
-             else
-                 index=searchPartition(nums,target,low,mid-1);
+            low=leftLow;
+            high=midVal;
         }
-        //System.out.println(index);
-        return index;
-       
+        else if(midVal<endHigh && target>=nums[midVal+1] && target<=nums[endHigh])
+        {
+            low=midVal+1;
+            high=endHigh;
+        }
+        else return -1;
+                
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+                
             
+        }
+        return -1;
+        
+        
+        
     }
-    
 }
