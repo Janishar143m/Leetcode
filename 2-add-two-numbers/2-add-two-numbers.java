6f778400a926b70699552d550fda2530d956ce05
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-08-30 08:51:16 +0530

    Time: 4 ms (37.47%), Space: 48.2 MB (27.12%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,75 +9,56 @@
  * }
  */
 class Solution {
-    
-    public ListNode reverseList(ListNode head)
-    {
-        ListNode temp=null,next=null;
-        
-            while(head!=null)
-            {
-                next=head.next;
-                head.next=temp;
-                temp=head;
-                head=next;
-            }
-        return temp;
-    }
-    
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        //l1=reverseList(l1);
-        //l2=reverseList(l2);
-        ListNode result=null,head=null;
+        ListNode result=null;
+        ListNode resultHead=null;
         int carry=0;
         while(l1!=null && l2!=null)
         {
-            
-            if(result==null)
+            int tot=(l1.val+l2.val+carry);
+            int sum=tot%10;
+            ListNode temp=new ListNode(sum);
+            carry=tot/10;
+            if(resultHead==null)
             {
-                result=new ListNode();
-                head=result;
-                
+                resultHead=temp;
+                result=temp;
             }
             else
             {
-                result.next=new ListNode();
+                result.next=temp;
                 result=result.next;
-            }   
-            int sum=(l1.val+l2.val+carry);
-           result.val=sum%10;
-           carry=sum/10;  
+            }
             l1=l1.next;
             l2=l2.next;
-           // prev=result;            
         }
         while(l1!=null)
         {
-            result.next=new ListNode();
-            result=result.next;
-            int sum=(l1.val+carry);
-            result.val=sum%10;
-            carry=sum/10;
-            //carry=result.val/10;
+             int tot=(l1.val+carry);
+            int sum=tot%10;
+            ListNode temp=new ListNode(sum);
+            carry=tot/10;
+                result.next=temp;
+                result=result.next;
+           
             l1=l1.next;
         }
-         while(l2!=null)
+           while(l2!=null)
         {
-            result.next=new ListNode();
-            result=result.next;
-             int sum=(l2.val+carry);
-            carry=sum/10;
-            result.val=sum%10;
-            //carry=result.val/10;
+             int tot=(l2.val+carry);
+            int sum=tot%10;
+            ListNode temp=new ListNode(sum);
+            carry=tot/10;
+           
+                result.next=temp;
+                result=result.next;
+            
             l2=l2.next;
         }
         if(carry!=0)
-        {
-            result.next=new ListNode();
-            result=result.next;
-            result.val=carry;
-        }
-        return head;
+            result.next=new ListNode(carry);
+        return resultHead;
         
     }
 }
