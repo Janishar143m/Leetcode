0b844c8a7e90b931d531379d31243715314dea50
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-09-22 09:45:37 +0530

    Time: 144 ms (12.14%), Space: 119.1 MB (10.90%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,32 +1,19 @@
 class Solution {
     
-    Queue<Point> queue=new PriorityQueue<>((a,b)->new Double(Math.sqrt(Math.pow(a.x,2)+Math.pow(a.y,2))).compareTo(new Double(Math.sqrt(Math.pow(b.x,2)+Math.pow(b.y,2)))));
+    Queue<int[]> queue=new PriorityQueue<>((a,b)->new Double(Math.sqrt(Math.pow(a[0],2)+Math.pow(a[1],2))).compareTo(new Double(Math.sqrt(Math.pow(b[0],2)+Math.pow(b[1],2)))));
     
     public int[][] kClosest(int[][] points, int k) {
         
         int [][] result=new int[k][2];
         
         for(int i=0;i<points.length;i++)
-            queue.offer(new Point(points[i][0],points[i][1]));
+            queue.offer(points[i]);
         
         for(int i=0;i<k;i++)
-        {
-            Point p=queue.poll();
-            result[i][0]=(int)p.x;
-             result[i][1]=(int)p.y;
-        }   
+           result[i]=queue.poll();
         
         return result;
         
     }
-    class Point
-    {
-        int x;
-        int y;
-        Point(int x,int y)
-        {
-            this.x=x;
-            this.y=y;
-        }
-    }
+    
 }
