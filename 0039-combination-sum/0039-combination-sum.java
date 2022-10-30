92d0b4d898204e2cbab67bc132289163979fac85
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2022-10-30 11:02:29 +0530

    Time: 9 ms (24.19%), Space: 42.2 MB (99.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -5,7 +5,6 @@ class Solution {
         List<List<Integer>> combinations=new ArrayList<>();
          List<List<Integer>> result=new ArrayList<>();
         findCombinations(candidates,0,target,list,combinations);
-        //System.out.println("Result combinations size is:"+combinations.size());
         return combinations;
         
         
@@ -29,12 +28,19 @@ class Solution {
         else
         {
         
+        if(target-candidates[index]>=0)
+        {
+            list.add(candidates[index]);
+            findCombinations(candidates,index+1,target-candidates[index],list,combinations);
+            findCombinations(candidates,index,target-candidates[index],list,combinations);
+            list.remove(list.size()-1);
+
             
-        list.add(candidates[index]);
+        }    
+        
         //System.out.println(list);    
-        findCombinations(candidates,index+1,target-candidates[index],list,combinations);
-        findCombinations(candidates,index,target-candidates[index],list,combinations);
-        list.remove(list.size()-1);
+        
+        
         findCombinations(candidates,index+1,target,list,combinations);
             
         }     
