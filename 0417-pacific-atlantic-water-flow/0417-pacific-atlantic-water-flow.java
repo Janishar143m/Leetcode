005444bb33db335f6466644322ebe5aeca8bba45
Janishar143m <74554319+Janishar143m@users.noreply.github.com>
2023-01-14 08:32:47 +0530

    Time: 4 ms (97.91%), Space: 43.5 MB (78.19%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,55 +1,47 @@
 class Solution {
-    boolean atlantic;
-    boolean pacific;
-    public List<List<Integer>> pacificAtlantic(int[][] heights) 
-    {
-       List<List<Integer>> result=new ArrayList<>();
-        int row=heights.length;
-        int col=heights[0].length;
-        Set<String> set;
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
-            {
-                pacific=false;
-                atlantic=false;
-                set=new HashSet<>();
-                checkCell(i,j,row,col,heights,Integer.MAX_VALUE,set);
-                //System.out.println("Row:"+i+" Column:"+j);
-                //System.out.println("Path is:"+set);    
-                if(atlantic && pacific)
-                {
-                    result.add(Arrays.asList(i,j));
+
+    int[][] dir = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        int rows = heights.length, cols = heights[0].length;
+        boolean[][] pacific = new boolean[rows][cols];
+        boolean[][] atlantic = new boolean[rows][cols];
+
+        for (int i = 0; i < cols; i++) {
+            dfs(heights, 0, i, Integer.MIN_VALUE, pacific);
+            dfs(heights, rows - 1, i, Integer.MIN_VALUE, atlantic);
+        }
+
+        for (int i = 0; i < rows; i++) {
+            dfs(heights, i, 0, Integer.MIN_VALUE, pacific);
+            dfs(heights, i, cols - 1, Integer.MIN_VALUE, atlantic);
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    res.add(Arrays.asList(i, j));
                 }
             }
-        return result;
-        
-        
+        }
+        return res;
     }
-    
-    private void checkCell(int i,int j,int row,int col,int[][]heights,int prev_value,Set<String> traversed)
-    {
-        if(i<0||i>=row||j<0||j>=col||heights[i][j]>prev_value||(pacific && atlantic)||traversed.contains(i+" "+j))
-        {
-            return;
+
+    private void dfs(
+        int[][] heights,
+        int i,
+        int j,
+        int prev,
+        boolean[][] ocean
+    ) {
+        if (i < 0 || i >= ocean.length || j < 0 || j >= ocean[0].length) return;
+        if (heights[i][j] < prev || ocean[i][j]) return;
+
+        ocean[i][j] = true;
+        for (int[] d : dir) {
+            dfs(heights, i + d[0], j + d[1], heights[i][j], ocean);
         }
-        if(i==0||j==0)
-        {
-            pacific=true;
-            
-        }
-        if(i==row-1 || j==col-1)
-        {
-            atlantic=true;
-        }
-        traversed.add(i+" "+j);
-        if(atlantic && pacific)
-        {
-            return;
-        }   
-        checkCell(i-1,j,row,col,heights,heights[i][j],traversed);
-        checkCell(i+1,j,row,col,heights,heights[i][j],traversed);
-        checkCell(i,j-1,row,col,heights,heights[i][j],traversed);
-        checkCell(i,j+1,row,col,heights,heights[i][j],traversed);
-        
     }
 }
